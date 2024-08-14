882b319f2839a93901ff87d6be72e8b7d1acbff3
Sermael <sermael.efigenio@gmail.com>
2024-08-14 14:45:33 -0600

    loader update

--- loader.py
+++ loader.py
@@ -1,14 +1,11 @@
-import sys
 from maya import cmds
 import importlib
-
+import seAutoRig
+importlib.reload(seAutoRig)
 import seAutoRig.scripts.project as project
 import seAutoRig.ui as ui
 importlib.reload(ui)
 importlib.reload(project)
-
-main_path = project.main_path
-sys.path.append(main_path)
 
 if __name__ == "__main__":
     workspace_control_name = ui.AutoRig.get_workspace_control_name()
@@ -18,3 +15,4 @@ if __name__ == "__main__":
 
     auto_rig = ui.AutoRig()
     auto_rig.show()
+
